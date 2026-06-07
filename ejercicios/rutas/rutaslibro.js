5522b585a200be2bc06e7856f22ac267ef053071
samuel <samuelariascusano@gmail.com>
2026-06-07 20:03:55 -0300

    rutas y controladores definidos

--- ejercicios/rutas/rutaslibro.js
+++ ejercicios/rutas/rutaslibro.js
@@ -1,19 +1,19 @@
-import express from "express"
+import express from "express" // importamos el modulo de express
 import {
     mostrarInicio,
     mostrarLibros,
     mostrarFormulario,
     mostrarLibro
-} from "../controladores/controladores.js"
+} from "../controladores/controladores.js" // importamos las funciones de las rutas
 
 
 const router = express.Router() // creamos la variable "router" para crear las rutas de la pagina
 
 
-router.get("/", mostrarInicio)
-router.get("/libros", mostrarLibros)
-router.get("/libros/nuevo", mostrarFormulario)
-router.get("/libros/:id", mostrarLibro)
+router.get("/", mostrarInicio) // ruta 1 (pagina principal)
+router.get("/libros", mostrarLibros) // ruta 2 (listado de libros)
+router.get("/libros/nuevo", mostrarFormulario) // ruta 3 (formulario para agregar nuevos libros)
+router.get("/libros/:id", mostrarLibro) // ruta 4 (detalle de un libro)
 
 export {
     router
