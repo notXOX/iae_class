66fbb600b7640a3bfdc4617256ff279579f7bfda
samuel <samuelariascusano@gmail.com>
2026-06-12 14:14:04 -0300

    eliminacion de archivos

--- ejercicios/index.js
+++
@@ -1,5 +0,0 @@
-import { router } from "./rutas/rutaslibro.js" // importamos el router donde estan las rutas
-import { servidor } from "./config.js" // importamos la configuracion del servidor 
-
-servidor.use(router) // le decimos al servidor que use router
-servidor.listen(4000) // el servidor se levanta en el puerto 4000
